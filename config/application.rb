900e4d7a10eefa2a82c9f1f927161052f0b2600e
Adam <adamadolfo8@gmail.com>
2021-01-12 15:06:16 -0500

    did not fix bug yet

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DoggydatesBackend
+module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
@@ -27,6 +27,6 @@ module DoggydatesBackend
           )
       end
     end
-    
+
   end
 end
