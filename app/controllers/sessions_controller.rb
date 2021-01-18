3ab68e9d3ea63534dad3bc874ff6edb979ba528f
Adam <adamadolfo8@gmail.com>
2021-01-18 15:41:39 -0500

    persisting login and signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
 
     def login
+        
         @owner = Owner.find_by(email: params[:email])
         .try(:authenticate, params[:password])
         if @owner
-            session[:user_id] = @user.id
-            render :json => @user.to_json(include: [:dogs])
+            render json: @owner.to_json(include: [:dogs] )
         end
     end
     
