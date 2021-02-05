aa43a431483b7102c6f586f93ec28ca4f80a1ab9
Adam <adamadolfo8@gmail.com>
2021-02-05 11:05:29 -0500

    working match making but need to include the hidden request feature

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,6 @@
 class MatchesController < ApplicationController
     def create
-        
+        Match.create(owner_id: params[:owner_id], friend_id: params[:friend_id])
     end
 
     def destroy
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,4 @@
 class Match < ApplicationRecord
     belongs_to :owner
-    belongs_to :friend, :class_name => "owner"
+    belongs_to :friend, :class_name => "Owner"
 end
