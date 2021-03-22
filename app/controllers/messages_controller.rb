43a54ba9ec298714e03ecb295f7961175012da0c
Adam <adamadolfo8@gmail.com>
2021-03-22 18:02:27 -0400

    working on messages in backend

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,10 @@ class MessagesController < ApplicationController
 
 
     def create
-        byebug
-        Message.create()
+       
+        @conversation = Conversation.find(params[:conversation_id])
+        @message = Message.create(conversation_id: @conversation.id, body: params[:body], owner_id: params[:owner_id])
+
+        render json: @conversation.to_json(include: [:messages] )
     end
 end
